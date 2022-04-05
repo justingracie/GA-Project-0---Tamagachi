b724412b03f6a24514d60d2fa093b3f45855e01c
justingracie <j.m.gracie@gmail.com>
2022-04-05 11:46:57 -0600

    updated some styling

--- app.js
+++ app.js
@@ -8,60 +8,65 @@ const hungryness = document.getElementById('hunger')
 let bored = 0
 const boredom = document.getElementById('bored')
 
-
-//sample counter method --->
-// set one get age count that increments: then displays Age on the screen.
+let sleep = 0;
+const sleepiness = document.getElementById('sleepy');
 
 
 // AGE Counter------>
-
-
 
 setInterval(()=>{
     age++
     ageValue.innerHTML = age;
 }, 10000)
-
 // age increasing by 1 every 10 seconds
 
 
 //HUNGER Counter ----->
 
-
 setInterval(()=>{
     hunger = Math.min(hunger +1, 10);
     hungryness.innerHTML = `${hunger} /10`;
 }, 1000);
+//hunger increases by 1 every second
 
 
 //Bored Counter ----->
-
 
 setInterval(()=>{
     bored = Math.min(bored +1, 10);
     boredom.innerHTML = `${bored} /10`
 }, 1000)
+//boredom increase by 1 every second
+
 
 //Sleepiness Counter ----->
-
-let sleep = 0;
-const sleepiness = document.getElementById('sleepy');
 
 setInterval(() =>{
     sleep = Math.min(sleep +=1, 10);
     sleepiness.innerHTML = `${sleep} /10`
 }, 1000)
+//sleepiness increase by 1 every second
+
+
+//FEED button -----> this button should decrement the hungriness by 2 on each click
+//PLAY BUTTON -----> this button should decrement the boredom by 5
+//LIGHTS OUT -----> this button should  decrement sleepiness by 3
+
+
+
+
+//START button ----->
+
+// start button should do a few things: 
+
+// 1. print the name entered in the  input box into a DIV 
+// 2. hide the name input box
+// 3. start the counters for the mame hunger, sleep, boredom, and age 
 
 
 
 
 
-//start BUtton name Section ----->
 
 // const start = document.getElementsByClassName('start');
-
 // console.log(start)
-// START BUTTON Pop up window input
-
-//use bootstrap modal
-//to get value 
