5e3075f86066b097c08dce121e5ff72795f3adbd
justingracie <j.m.gracie@gmail.com>
2022-04-05 09:11:31 -0600

    Age incrementer function working

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('hello world');
+//  --->GLOBAL VARIABLES<--- //
+let age = 0
+let ageValue= document.getElementById('age');
+
+
+//sample counter method --->
+// set one get age count that increments: then displays Age on the screen.
+
+
+// AGE INCREMENTER ------>
+
+
+console.log(ageValue)
+
+
+setInterval(()=>{
+    age++
+    ageValue.innerHTML = age;
+}, 100000)
+
+// age increasing by 1 every 10 seconds
+
+
+
+
+
+
+//start BUtton name Section ----->
+
+// const start = document.getElementsByClassName('start');
+
+// console.log(start)
+// START BUTTON Pop up window input
+
+//use bootstrap modal
+//to get value 
